8e30b757715f5661d4b205fee414536cd1fbffde
LeCun <2899610146@qq.com>
2022-01-16 17:06:25 +0800

    统计用户获赞数量，开发个人主页

---
+++ src/test/java/com/nowcoder/community/RedisTests.java
@@ -0,0 +1,114 @@
+package com.nowcoder.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.*;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class RedisTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testStrings() {
+        String redisKey = "test:count";
+        redisTemplate.opsForValue().set(redisKey, 1);
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+        System.out.println(redisTemplate.opsForValue().decrement(redisKey));
+    }
+
+    @Test
+    public void testHashes() {
+        String redisKey = "test:user";
+        redisTemplate.opsForHash().put(redisKey, "id", 1);
+        redisTemplate.opsForHash().put(redisKey, "username", "zhangsan");
+
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "id"));
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "username"));
+    }
+
+    @Test
+    public void testLists() {
+        String redisKey = "test:ids";
+        redisTemplate.opsForList().leftPush(redisKey, 101);
+        redisTemplate.opsForList().leftPush(redisKey, 102);
+        redisTemplate.opsForList().leftPush(redisKey, 103);
+
+        System.out.println(redisTemplate.opsForList().size(redisKey));
+        System.out.println(redisTemplate.opsForList().index(redisKey, 0));
+        System.out.println(redisTemplate.opsForList().range(redisKey, 0, 2));
+
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+    }
+
+    @Test
+    public void testSets() {
+        String redisKey = "test:teachers";
+        redisTemplate.opsForSet().add(redisKey, "aa", "bb", "cc", "dd");
+
+        System.out.println(redisTemplate.opsForSet().size(redisKey));
+        System.out.println(redisTemplate.opsForSet().pop(redisKey));
+        System.out.println(redisTemplate.opsForSet().members(redisKey));
+    }
+
+    @Test
+    public void testSortedSets() {
+        String redisKey = "test:student";
+
+        redisTemplate.opsForZSet().add(redisKey, "aa", 80);
+        redisTemplate.opsForZSet().add(redisKey, "bb", 90);
+        redisTemplate.opsForZSet().add(redisKey, "cc", 70);
+        redisTemplate.opsForZSet().add(redisKey, "dd", 40);
+        redisTemplate.opsForZSet().add(redisKey, "ee", 20);
+
+        // 统计Zset中多少数据
+        System.out.println(redisTemplate.opsForZSet().zCard(redisKey));
+        System.out.println(redisTemplate.opsForZSet().score(redisKey, "aa"));   // 查询aa多少分
+        System.out.println(redisTemplate.opsForZSet().rank(redisKey, "bb"));    // 查询aa排多少名,由小到大
+        System.out.println(redisTemplate.opsForZSet().reverseRank(redisKey, "bb"));    // 查询aa排多少名,由大到小
+        System.out.println(redisTemplate.opsForZSet().range(redisKey, 0, 2));    // 查询排名0-2的数据,由小到大
+    }
+
+    @Test
+    public void testKey() {
+        redisTemplate.delete("test:user");
+        System.out.println(redisTemplate.hasKey("test:user"));
+
+        System.out.println(redisTemplate.expire("test:student", 10, TimeUnit.SECONDS));
+    }
+
+    // 多次访问同一个key
+    @Test
+    public void testBoundOperations() {
+        String redisKey = "test:count";
+        BoundValueOperations operations = redisTemplate.boundValueOps(redisKey);
+        operations.increment();
+    }
+
+    @Test
+    public void testTransactional() {
+        Object obj = redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String redisKey = "test:tx";
+                operations.multi();
+                operations.opsForSet().add(redisKey, "zhangsan", "lisi", "wangwu");
+                System.out.println(operations.opsForSet().members(redisKey));
+                return operations.exec();
+            }
+        });
+        System.out.println(obj);
+    }
+}
